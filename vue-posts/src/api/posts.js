4f1a2be5326801f5e5b8126da084a5cfa229556a
Racconeee <117073015+Racconeee@users.noreply.github.com>
2024-08-06 09:34:09 +0900

    08060933

--- vue-posts/src/api/posts.js
+++ vue-posts/src/api/posts.js
@@ -3,7 +3,7 @@ const posts = [
 	{ id: 2, title: '제목2', content: '내용2', createdAt: '2020-02-01' },
 	{ id: 3, title: '제목3', content: '내용3', createdAt: '2020-03-01' },
 	{ id: 4, title: '제목4', content: '내용4', createdAt: '2020-04-01' },
-	{ id: 5, title: '제목5', content: '내용5', createdAt: '2020-05-01' },
+	{ id: 7, title: '제목5', content: '내용5', createdAt: '2020-05-01' },
 ];
 
 export function getPosts() {
